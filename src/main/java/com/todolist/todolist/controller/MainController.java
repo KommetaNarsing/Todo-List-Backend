21aeedad944621fd84d5fa6a6e12180b13b5e8d3
chinna <kommetan@gmail.com>
2022-02-10 00:22:22 -0500

    Working DB calls

--- src/main/java/com/todolist/todolist/controller/MainController.java
+++ src/main/java/com/todolist/todolist/controller/MainController.java
@@ -6,18 +6,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@CrossOrigin
 @RequestMapping(path = "/todo")
 public class MainController {
 
     @Autowired
+
     JdbcTemplate jdbcTemplate;
 
-    @CrossOrigin
     @PostMapping(path = "/add",consumes = {"application/json"})
     public String addItem(@RequestBody TodoItem item){
         new TodoItemDao(jdbcTemplate).insert(item);
         return "Success";
+    }
+
+    @GetMapping(path = "/list")
+    public List<TodoItem> getItems(){
+       return  new TodoItemDao(jdbcTemplate).fetch();
     }
 
 }
--- src/main/java/com/todolist/todolist/dao/TodoItemDao.java
+++ src/main/java/com/todolist/todolist/dao/TodoItemDao.java
@@ -2,11 +2,16 @@ package com.todolist.todolist.dao;
 
 import com.todolist.todolist.entity.TodoItem;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.PreparedStatementSetter;
+import org.springframework.jdbc.core.ResultSetExtractor;
+import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.List;
 
 public class TodoItemDao {
 
@@ -28,5 +33,21 @@ public class TodoItemDao {
         if (rows > 0) {
             System.out.println("A new row has been inserted.");
         }
+    }
+
+    public List<TodoItem> fetch(){
+        String sql = "SELECT * from todo_item";
+        return  jdbcTemplate.query(sql, new RowMapper<TodoItem>() {
+            @Override
+            public TodoItem mapRow(ResultSet rs, int rowNum) throws SQLException {
+                TodoItem todoItem = new TodoItem();
+                todoItem.setTodoItemId(rs.getString("id"));
+                todoItem.setDescription(rs.getString("description"));
+                todoItem.setUserId(rs.getString("user_id"));
+                todoItem.setTimeStamp(rs.getLong("timestamp"));
+                return todoItem;
+            }
+        });
+
     }
 }
